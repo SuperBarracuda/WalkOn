96fdc609594cd7e2192d9c4e59b63d0fe25e5fc5
Tris <tristram.dev@gmail.com>
2021-05-17 13:19:15 +0100

    Fix complilation issues.

---
+++ WalkOn/WalkOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkOnApp.swift
+//  WalkOn
+//
+//  Created by Tristram Bates on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WalkOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
